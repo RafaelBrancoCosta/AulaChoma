dd29ccba59231995f8efdca007d96c750522c77f
Rafael de Branco Costa <rafaelbrancocosta30@gmail.com>
2025-03-20 22:18:11 -0300

    Introdução a JavaScript
     conceito de função em JS e como colocar um botão e linkar ao JS.

---
+++ semana6/inferno.js
@@ -0,0 +1,23 @@
+// Código: JavaScript
+       /* var primeiroTipo;
+        let segundoTipo;
+        const terceiroTipo = 10;
+
+        var nome = 'Rafael';
+        var idade = 19;
+        var altura = 1.75;
+        
+        console.log(nome);
+        console.log(idade);
+        console.log(altura);
+
+        nome = idade;
+        console.log(nome);*/
+
+        function escrever(){
+            document.getElementById('texto').innerText = 'Oi, voce me picou!';
+            document.getElementById('imagem').src ="";
+        }
+
+
+    
