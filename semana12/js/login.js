ade2f2b296f66b9769171df16ed21b9654967582
RafaBranco <rafaelbrancocosta30@gmail.com>
2025-06-05 19:24:54 -0300

    sla

--- semana12/js/login.js
+++ semana12/js/login.js
@@ -1,19 +1,19 @@
 //autenticacao de login//
 
-document.getElementById("loginForm").addEventListener("submit", function (e) {
-    e.preventDefault();
-  
-    const login = document.getElementById("login").value.trim();
-    const senha = document.getElementById("senha").value;
-  
-    const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
-  
-    const usuario = usuarios.find(u => u.login === login && u.senha === senha);
-  
-    if (usuario) {
-      localStorage.setItem("usuarioLogado", login);
-      window.location.href = "dashboard.html";
-    } else {
-      document.getElementById("mensagem").textContent = "Login ou senha incorretos.";
+const botao = document.getElementById('entrar');
+botao.addEventListener('click',
+    function (){
+        const usuario = JSON.parse(localStorage.getItem("usuarios")) || [];
+        let login = document.getElementById("login").value;
+        let senha = document.getElementById("senha").value;
+
+        const logado = usuario.find(user => user.login === login && user.senha === senha);
+    
+        if(logado){
+            localStorage.setItem('userLogado', login);
+            window.location.href = "dashbord.html";
+        }else{
+            document.getElementById('mensagemErro').textContent = "Login e Senha incorretos";
+        }
     }
-  });
+);
